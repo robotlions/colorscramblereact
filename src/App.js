63bdd7a6743997cdfc4070055050d4e72060445d
Chad M <chadmusick@gmail.com>
2023-11-27 07:58:29 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 function App() {
+  let blockRow = (
+    <div class="row">
+      <div class="col-4 col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+      <div class="col-4 col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+      <div class="col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+      <div class="col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+      <div class="col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+      <div class="col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+      <div class="col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+      <div class="col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+      <div class="col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+      <div class="col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+      <div class="col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+      <div class="col-4 col-md-1">
+        <div class="colorBox"></div>
+      </div>
+    </div>
+  );
+
+  function rando() {
+    return Math.floor(Math.random() * 255) + 1;
+  }
+
+  function shuffleColors() {
+    let classEntries = document.querySelectorAll(".colorBox");
+    for (let i = 0; i < classEntries.length; i++) {
+      let randomColor = `rgb(${rando()}, ${rando()}, ${rando()})`;
+      classEntries[i].style.backgroundColor = randomColor;
+      // classEntries[i].style.backgroundImage = `linear-gradient(white, ${randomColor})`
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div id="mainButtonWrapper">
+        <button
+          id="mainButton"
+          className="btn btn-warning"
+          onClick={() => shuffleColors()}
         >
-          Learn React
-        </a>
-      </header>
+          Shuffle Colors
+        </button>
+      </div>
+      {blockRow}
+      {blockRow}
+      {blockRow}
+      {blockRow}
+      {blockRow}
+      {blockRow}
+      {blockRow}
+      {blockRow}
+      {blockRow}
+      {blockRow}
     </div>
   );
 }
